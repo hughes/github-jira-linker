56853f3b02bbd775532170223db9a6fc5d85c921
Matt Hughes <hughesm02@gmail.com>
2014-10-31 13:32:50 -0400

    Rewrote to not interfere with existing links

--- js/linker.js
+++ js/linker.js
@@ -1,25 +1,75 @@
-var JIRA_LINK, PROJECTS;
+(function () {
+  'use strict';
+  var JIRA_LINK, PROJECTS;
+  var issue_regex_str, issue_regex, selector;
 
-var regex_str, regex, selector;
-
-function wrapJiraLink(index, el) {
-    var new_html = $(el).html().replace(regex,
-        '<a href="' + JIRA_LINK + '/browse/$1" target="_blank">$1</a>');
-    $(el).html(new_html);
-}
-
-chrome.storage.sync.get({
-  subdomain: null,
-  projects: null
-}, function (options) {
-  if (options.subdomain && options.projects) {
-    JIRA_LINK = 'https://' + options.subdomain + '.atlassian.net';
-    PROJECTS = options.projects;
-
-    regex_str = '((' + PROJECTS.join('|') + ')-\\d+)';
-    regex = new RegExp(regex_str, 'g');
-    selector = 'p:regex("' + regex_str + '")';
-
-    $(selector).each(wrapJiraLink);
+  function walk(node) {
+    var child = node.firstChild, next;
+    while (child) {
+      next = child.nextSibling;
+      walk(child);
+      child = next;
+    }
+    if (shouldWrapLink(node)) {
+      wrapJiraLink(node);
+    }
   }
-});
+
+  function shouldWrapLink(node) {
+    return (
+      node.nodeType === Node.TEXT_NODE &&
+      issue_regex.test(node.textContent) &&
+      !hasInteractiveParent(node)
+    );
+  }
+
+  function hasInteractiveParent(node) {
+    // partial implementation of
+    // https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/Content_categories
+    if (node.parentNode) {
+      if (node.parentNode.nodeName === 'A') {
+        return true;
+      } else {
+        return hasInteractiveParent(node.parentNode);
+      }
+    }
+    return false;
+  }
+
+  function wrapJiraLink(originalNode) {
+    var parent = originalNode.parentNode;
+    var matches = originalNode.textContent.match(issue_regex);
+    var textChunks = originalNode.textContent.split(issue_regex);
+    var newLinkNode, newTextNode;
+
+    textChunks.forEach(function (chunk, i) {
+      newTextNode = document.createTextNode(chunk);
+      parent.insertBefore(newTextNode, originalNode);
+
+      if (i < matches.length) {
+        newLinkNode = document.createElement('A');
+        newLinkNode.setAttribute('href', JIRA_LINK + '/browse/' + matches[i]);
+        newLinkNode.setAttribute('target', '_blank');
+        newLinkNode.innerHTML = matches[i];
+        parent.insertBefore(newLinkNode, originalNode);
+      }
+    });
+
+    parent.removeChild(originalNode);
+  }
+
+  chrome.storage.sync.get({
+    subdomain: null,
+    projects: null
+  }, function (options) {
+    if (options.subdomain && options.projects) {
+      JIRA_LINK = 'https://' + options.subdomain + '.atlassian.net';
+      PROJECTS = options.projects;
+
+      issue_regex_str = PROJECTS.join('-\\d+|') + '-\\d+';
+      issue_regex = new RegExp(issue_regex_str, 'g');
+
+      walk(document.body);
+    }
+  });
+})();
