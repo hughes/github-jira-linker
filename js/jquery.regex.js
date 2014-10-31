d7f14695c5ab1bd6161dddcebbbfc7ffb858657b
Matt Hughes <hughesm02@gmail.com>
2014-10-31 14:32:08 -0400

    Removed jquery.regex.js

--- js/jquery.regex.js
+++
@@ -1,8 +0,0 @@
-jQuery.extend(
-    jQuery.expr[':'], {
-        regex: function(a, i, m, r) {
-            var r = new RegExp(m[3], 'i');
-            return r.test(jQuery(a).text());
-        }
-    }
-);
